f04e773c6955f2b0fdd63f0d1a87a0714dc53a9d
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2019-08-01 09:05:47 -0400

    test guessing blogdown

---
+++ tests/testthat/test-blogdown.R
@@ -0,0 +1,29 @@
+with_dir <- function(path, x) {
+  owd <- setwd(path)
+  on.exit(setwd(owd))
+  eval(substitute(x))
+}
+
+test_that("blogdown is guessed correctly", {
+  blogdown_dir <- file.path(tempdir(), "blogdown", "content", "post")
+  dir.create(blogdown_dir, recursive = TRUE)
+  dir.create(file.path(blogdown_dir, "..", "..", "layouts"))
+  dir.create(file.path(blogdown_dir, "..", "..", "static"))
+  cat('', file = file.path(blogdown_dir, "..", "..", "config.toml"))
+
+
+  blogdown_exp <- normalizePath(file.path(tempdir(), "blogdown"))
+  expect_equal(find_config(blogdown_dir), blogdown_exp)
+
+  with_dir(blogdown_dir, expect_true(guess_blogdown()))
+
+
+  cat('baseURL = "/"', file = file.path(blogdown_dir, "..", "..", "config.toml"))
+  expect_equal(find_config(blogdown_dir), blogdown_exp)
+  with_dir(blogdown_dir, expect_true(guess_blogdown()))
+
+  expect_null(find_config(tempdir()))
+  with_dir(tempdir(), expect_false(guess_blogdown()))
+
+  unlink(file.path(tempdir(), "blogdown"), recursive = TRUE)
+})
