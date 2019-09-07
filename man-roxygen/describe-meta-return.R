13a72a5fef51f0984a3c1e17e6e2beed8fa2a8c3
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2019-09-07 00:31:10 -0400

    <meta> tags not <metadata>

--- man-roxygen/describe-meta-return.R
+++ man-roxygen/describe-meta-return.R
@@ -1,4 +1,4 @@
-#' @return A `meta` object, or a set of `<metadata>` HTML tags inside an HTML
+#' @return A `meta` object, or a set of `<meta>` HTML tags inside an HTML
 #'   `<head>` tag. For use in [rmarkdown::html_document()], [shiny::runApp()],
 #'   or other HTML locations.
 #'
