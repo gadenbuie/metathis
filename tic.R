9ccb4e2c366aa4c96ad89d8fd43cec20dddf4ed3
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2019-09-10 21:29:22 -0400

    travis: another try to install from github

--- tic.R
+++ tic.R
@@ -1,6 +1,9 @@
 do_package_checks()
 
 if (ci_on_travis()) {
-  step_install_github("gadenbuie/pkgdown@meta-social-cards")
+  get_stage("install") %>%
+    add_step(
+      step_install_github("gadenbuie/pkgdown@meta-social-cards")
+    )
   do_pkgdown()
 }
