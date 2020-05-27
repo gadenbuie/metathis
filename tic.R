36b2820efb72cea7482cfb67767054e4c14fa0e2
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-05-26 20:41:49 -0400

    Move CI to GitHub Actions
    Closes #8

--- tic.R
+++ tic.R
@@ -1,9 +1,8 @@
+# installs dependencies, runs R CMD check, runs covr::codecov()
 do_package_checks()
 
-if (ci_on_travis()) {
-  get_stage("install") %>%
-    add_step(
-      step_install_github("gadenbuie/pkgdown@meta-social-cards")
-    )
+if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
+  # creates pkgdown site and pushes to gh-pages branch
+  # only for the runner with the "BUILD_PKGDOWN" env var set
   do_pkgdown()
 }
