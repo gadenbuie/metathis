7c61e9375aef6c4876e19cef43e8c62716d16a0f
Garrick Aden-Buie <garrick@adenbuie.com>
2021-06-28 11:48:03 -0400

    Update test to catch rmarkdown < 2.9

--- tests/testthat/test-rmd.R
+++ tests/testthat/test-rmd.R
@@ -17,7 +17,6 @@ test_that("knit_print() and include_meta() work in Rmd", {
 
 test_that("Doesn't create empty directory for non self-contained RMarkdown", {
   skip_if_not(rmarkdown::pandoc_available("1.12.3"))
-  skip_if_not(has_package_version("rmarkdown", "2.9"))
 
   temp_dir <- tempfile("metathis-rmd")
   dir.create(temp_dir)
@@ -26,13 +25,14 @@ test_that("Doesn't create empty directory for non self-contained RMarkdown", {
   test_rmd_src <- test_path("rmd", "test-not-self-contained.Rmd")
   test_rmd <- file.path(temp_dir, "test.Rmd")
   file.copy(test_rmd_src, test_rmd)
-  rmarkdown::render(test_rmd, quiet = TRUE)
+  expect_silent(rmarkdown::render(test_rmd, quiet = TRUE))
   out <- readLines(file.path(temp_dir, "test.html"))
 
   has_string <- function(str, n = 1L) {
     sum(grepl(str, out, fixed = FALSE)) == n
   }
 
+  skip_if_not(has_package_version("rmarkdown", "2.9"))
   expect_true(has_string('<meta method="KNIT_PRINT" ?/?>'))
   expect_true(has_string('<meta method="knit_print\\(2\\)" ?/?>'))
   expect_true(has_string('<meta method="INCLUDE_META\\(\\)" ?/?>'))
